850f7dca0b6e9deeea659c3a5a4e0c030423de43
selma tareb <94302570+selma001@users.noreply.github.com>
2022-12-02 06:57:55 +0100

    i added a profil page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,10 @@
 import { useUser } from "hooks";
+import Confirmation from "Pages/Confirmation";
 import Error404 from "Pages/Errors/Error404";
 import Home from "Pages/Home";
 import LogIn from "Pages/LogIn";
 import SignUp from "Pages/SignUp";
+import Profil from "Pages/profile";
 import { Navigate, useRoutes } from "react-router-dom";
 
 const Router = () => {
@@ -21,6 +23,10 @@ const Router = () => {
             element: user ? <Navigate to="/admin" /> : <SignUp />,
         },
         {
+            path: "/profil",
+            element: user ? <Navigate to="/admin" /> : <Profil />,
+        },
+        {
             path: "/admin",
             element: user ? <Error404 /> : <Navigate to="/login" />,
         },
@@ -28,6 +34,11 @@ const Router = () => {
             path: "*",
             element: <Error404 />,
         },
+        {
+            path: "/confirmation",
+            element: user ? <Navigate to="/admin" /> : <Confirmation />,
+        },
+        
     ]);
 };
 export default Router;
