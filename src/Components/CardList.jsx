0110bd67486183021f06612c3bf07de13f671f72
selma tareb <94302570+selma001@users.noreply.github.com>
2022-12-02 18:42:36 +0100

    profile again

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -5,61 +5,88 @@ import DateProfil from "./DateProfil";
 
 export default function CardList({ List }) {
   return (
-     
     <Card
       sx={{
-       
         color: "primary.main",
         backgroundColor: "primary.main",
         marginTop: "2em",
         marginRight: "-1em",
         marginLeft: "-1em",
-        padding:"1em",
-        paddingBottom:"2em",
-        paddingRight:"3em",paddingLeft:"3em",borderRadius:"15px"
-        
+        padding: "1em",
+        paddingBottom: "2em",
+        paddingRight: "3em",
+        paddingLeft: "3em",
+        borderRadius: "15px",
       }}
     >
-         <Typography  sx={{fontSize: "1.3em", margin: "auto",color:"white" }}>
-         History of Visits :
-           </Typography>
-          <Box sx={{
-            display: "flex",
-            justifyContent:"space-between"
+      <Typography sx={{ fontSize: "1.3em", margin: "auto", color: "white" }}>
+        History of Visits :
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <DateProfil num="01" month="sept." />
+        <DateProfil selected num="26" month="oct." />
+        <DateProfil num="11" month="oct." />
+        <DateProfil num="02" month="nov." />
+        <DateProfil num="22" month="nov." />
+        <DateProfil num="15" month="nov." />
+      </Box>
+      {List.map((element, i) => (
+        <Box
+          key={i}
+          sx={{
+            background: "white",
+            padding: "1em",
+            borderRadius: "15px",
+            marginTop: "1em",
+            display: "flex"
           }}
-          >
-          <DateProfil num="01" month="sept." />
-          <DateProfil selected num="26" month="oct." />
-          <DateProfil num="11" month="oct." />
-          <DateProfil num="02" month="nov." />
-          <DateProfil num="22" month="nov." />
-          <DateProfil num="15" month="nov." />
-          </Box>
-      {List.map((element,i) => (
-        
-        <Box key={i} sx={{ background: "white", padding: "1em", borderRadius: "15px" , marginTop:"1em"}}>
-          <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+        >
+          <Box>
             {" "}
-            <Typography sx={{ fontWeight: "600" }} variant="h6" align="left">
-              {   element.date}
-           
+            <Typography
+              sx={{ fontWeight: "600", fontSize: "16px" }}
+              align="left"
+            >
+              {element.date}
             </Typography>
-            <Typography sx={{ fontSize: "0.5em", margin: "auto" }}>
-              {   element.hospital}
-           
+            <Typography sx={{ fontSize: "8px", margin: "auto" }}>
+              {element.hospital}
+            </Typography>
+            <Typography
+              sx={{
+                color: "primary.main",
+                marginBottom: "1em",
+                fontSize: "0.5em",
+              }}
+            >
+              {element.doctor}
             </Typography>
           </Box>
-          <Typography
-            sx={{
-              color: "primary.main",
-              marginBottom: "1em",
-              fontSize: "0.5em",
-            }}
-          >
-            {element.doctor}
-            
-          </Typography>
-          <Box sx={{ display: "flex", justifyContent: "end" }}>
+          <Box sx={{}}>
+          <Button
+              sx={{
+                borderRadius: "10px",
+                marginRight: "0.01em",
+                backgroundColor: "rgba(176, 34, 243, 0.22)",
+                color:"#B022F3",
+                padding: "3em",
+                paddingTop: "0.3em",
+                paddingBottom: "0.3em",
+                fontSize: "14px",
+                border: "0.651852px solid #B022F3"
+              }}
+              onClick={() => {
+                console.log("Reading more");
+              }}
+            >
+              {" "}
+              <Typography sx={{ color: "#B022F3", fontSize:"14px" }}>Physical Analysis</Typography>{" "}
+            </Button>
             <Button
               sx={{
                 borderRadius: "10px",
