9fc2b2de7e6efaa70b009b70efc3c4d703a2101e
selma tareb <94302570+selma001@users.noreply.github.com>
2022-12-02 09:30:33 +0100

    heyo

--- src/Pages/profile/index.jsx
+++ src/Pages/profile/index.jsx
@@ -7,6 +7,7 @@ import {
     Grid,
     Link as MuiLink,
     Typography,
+    Modal
 } from "@mui/material";
 
 import GitBetter from "images/GitBetter.svg";
@@ -17,21 +18,24 @@ import ProfilCard from "Components/ProfilCard";
 
 
 export default function Profil(){
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return (
-        
-            <Container
-             sx={{
-                px:3,
+
+        <Container
+         sx={{
+            px:3,
                
-             }}
-            >
-                <Box sx={{display: "flex", justifyContent:"space-between", pt: "1rem" }}>
+            }}
+        >
+            <Box sx={{display: "flex", justifyContent:"space-between", pt: "1rem" }}>
               <Box  component="img" alt="Logo" src={humburger} sx={{width:"25.24px"}} ></Box>
               <Box component="img" alt="Logo" src={GitBetter} sx={{width:"101.41px"}}></Box>
-              </Box>
-               <ProfilCard  />
+            </Box>
+            <ProfilCard />
             
-            </Container>
+        </Container>
             
         
     )
