27152a31f288c8d2322916b7b06da6b7d30cbe4d
selma tareb <94302570+selma001@users.noreply.github.com>
2022-12-02 23:03:25 +0100

    yay kmlna

---
+++ src/Components/drugs.jsx
@@ -0,0 +1,58 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Drugs({
+  color = "#FF844F",
+  bgcolor = "rgba(255, 132, 79, 0.21)",
+  border = " 1px solid #FF844F",
+  drug,
+  label,
+  img,
+}) {
+  return (
+      <Box
+        sx={{
+          border: "1px solid #F0F0F0",
+          width: "148px",
+          p: 1,
+          borderRadius: 3,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold" }}>{drug}</Typography>
+          <Typography sx={{ color, fontSize: "8px" }}>3 x Day</Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            mt: 1,
+          }}
+        >
+          <img src={img} width="20px" />
+          <Box
+            sx={{
+              width: "45.13px",
+              height: "12.14px",
+              bgcolor,
+              border,
+              borderRadius: "12px",
+              fontSize: "8px",
+              color,
+              textAlign: "center",
+              ml: "auto",
+              pb: 1.5,
+            }}
+          >
+            {label}
+          </Box>
+        </Box>
+      </Box>
+  );
+}
--- src/Pages/analysis/index.jsx
+++ src/Pages/analysis/index.jsx
@@ -1,7 +1,55 @@
 import { Box } from "@mui/material";
+import Green from "images/green-drug.svg";
+import Red from "images/red-drug.svg";
+import AppBar from "Components/Appbar";
+import ProfilCard from "Components/ProfilCard";
+import Drugs from "Components/drugs";
+import QRBar from "Components/QRBar";
 
 export default function Analysis() {
-   return (
-    <Box>selma</Box>
-   )
+  return (
+    <Box>
+      <Box sx={{ p: 2 }}>
+        <AppBar />
+        <Box sx={{ p: 3 }}>
+          <ProfilCard />
+
+          <Box
+            sx={{
+              mt: 5,
+              display: "flex",
+            }}
+          >
+            <Box sx={{ mr: 2 }}>
+              <Drugs drug="Saroukh" img={Red} label="missed !" />
+              <br />
+              <Drugs drug="Saroukh" img={Red} label="missed !" />
+            </Box>
+
+            <Box>
+              <Drugs
+                drug="L’erica"
+                img={Green}
+                label="on time"
+                bgcolor="rgba(61, 182, 187, 0.21)"
+                border="1px solid #3DB6BB"
+                color="#3DB6BB"
+              />
+              <br />
+              <Drugs
+                drug="L’erica"
+                img={Green}
+                label="on time"
+                bgcolor="rgba(61, 182, 187, 0.21)"
+                border="1px solid #3DB6BB"
+                color="#3DB6BB"
+              />
+            </Box>
+            <Box></Box>
+          </Box>
+        </Box>
+      </Box>
+      <QRBar/>
+    </Box>
+  );
 }
